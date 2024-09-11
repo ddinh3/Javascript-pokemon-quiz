4756a304646ff022a8eb6d21612dcb1a01266ca2
ddinh3 <132150804+ddinh3@users.noreply.github.com>
2024-09-11 02:48:35 -0700

    -answer works lowercase or uppercase -added 3 more questions TODO: add a function to tally up the right /wrong answers

--- quiz.js
+++ quiz.js
@@ -7,10 +7,30 @@ console.log(
 const question1 = prompt(
   "What is the starter pokemon that professor Oak gave to Ash? "
 );
-const question1answer = "Pikachu";
+const question1Answer = "PIKACHU";
 
-if (question1.toUpperCase === question1answer.toUpperCase) {
+if (question1.toUpperCase() === question1Answer) {
   console.log("You got it correct!");
 } else {
-  console.log("You got it wrong...");
+  console.log("You got it wrong... but lets continue your quizz");
+}
+
+const question2 = prompt("Question 2: What pokemon evolves into Gyarados? ");
+const question2Answer = "MAGIKARP";
+
+if (question2.toUpperCase() === question2Answer) {
+  console.log("You got it correct!");
+} else {
+  console.log("You got it wrong... ");
+}
+
+const question3 = prompt(
+  "Which Pokémon is known as the “Flame Pokémon” and is the final evolution of Charmander? "
+);
+const question3Answer = "CHARIZARD";
+
+if (question3.toUpperCase() === question3Answer) {
+  console.log("Correct Answer!");
+} else {
+  console.log("wrong... answer...");
 }

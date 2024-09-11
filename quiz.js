6e376adc0f1a26a7d156fca7b02cce0c4bde1157
ddinh3 <132150804+ddinh3@users.noreply.github.com>
2024-09-11 01:47:31 -0700

    added basic if / else statement

--- quiz.js
+++ quiz.js
@@ -1,3 +1,16 @@
 const prompt = require("prompt-sync")();
 
-const value = prompt("Enter something: ");  // declaring a variable 
+console.log(
+  "Welcome trainer. So you want to be a pokemon professor? Let me quiz you. "
+);
+
+const question1 = prompt(
+  "What is the starter pokemon that professor Oak gave to Ash? "
+);
+const question1answer = "Pikachu";
+
+if (question1.toUpperCase === question1answer.toUpperCase) {
+  console.log("You got it correct!");
+} else {
+  console.log("You got it wrong...");
+}
